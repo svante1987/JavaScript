3afc84c192c332fd43231ac02e0270687b958a73
Svante Pålsson <70195788+svante1987@users.noreply.github.com>
2021-02-19 08:24:05 +0100

    skrev om debugging i main.js   | |   fixade så att min bakgrund i body följer med sidan och inte upprepas när man scrollar i css.

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-/* använder use strict så att jag inte lika lätt ska kunna göra slarvfel */
+/*Felsöka JS:
+Använder use strict så att jag inte lika lätt ska kunna göra slarvfel.
+Nedanför här så har jag lite kod som skall få min header att rulla med när jag scrollar, den funkar inte.
+När jag är inne i dev tools i chrome och kollar i konsollen så ser jag tre felmeddelanden.
+Felsöka html/css:
+När jag skriver min kod i css/html så har jag VS code och min browser uppe samtidigt,
+genom tillägget live server i VS code kan jag se min hemsida i browsern och kan exprimentera i
+css och html och direkt se resultatet på hemsidan. 
+Ett väldigt dynamiskt sätt att jobba och rätt kul faktiskt. :D*/
 "use strict";
 document.addEventListener("DOMContentLoaded", onPageLoad);
 
